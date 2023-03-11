88aa7189c2a246209148b4342b21e791300613ac
Jack Hammond <ham18028@byui.edu>
2023-03-11 15:17:59 -0700

    personal html

--- CSE 110/shopping_cart.py
+++ CSE 110/shopping_cart.py
@@ -1,9 +1,11 @@
 print('Welcome to the Shopping Cart Program!')
 print('')
-choice = 0
+choice = None
 cart = []
+prices = []
+amounts = []
 
-while choice != 5:
+while choice != '5':
     print('')
     print('Please select one of the following: ')
     print('1. Add item')
@@ -11,24 +13,39 @@ while choice != 5:
     print('3. Remove item')
     print('4. Compute total')
     print('5. Quit')
-    choice = int(input('Please enter an action: '))
-    if choice == 1:
+    choice = input('Please enter an action: ')
+    if choice == '1':
         item = input('What item would you like to add? ')
         cart.append(item)
-        print(f'\'{item}\' has been added to the cart.')
-    elif choice == 2:
+        price = float(input(f'What is the price of {item}? '))
+        prices.append(price)
+        amount = int(input(f'How much/many {item} do you want? '))
+        amounts.append(amount)
+        print(f'{amount} \'{item}\' has been added to the cart.')
+    elif choice == '2':
         print('The contents of the shopping cart are: ')
-        print(cart)
-    elif choice == 3:
-        print('I will add this soon.')
-    elif choice == 4:
-        print('I will add this soon.')
-    
-    
-
-   
-
-
+        for index, item in enumerate(cart):
+            print(f'{index + 1}. {amounts[index]} {item} - ${prices[index]:.2f}')
+    elif choice == '3':
+        item_removed = int(input('Which item would you like to remove? '))
+        num_removed = item_removed - 1
+        if item_removed <= len(cart):
+            cart.pop(num_removed)
+            prices.pop(num_removed)
+            amounts.pop(num_removed)
+            print('Item removed.')
+        else:
+            print('Sorry, that is not a valid item number.')
+    elif choice == '4':
+        total = 0
+        for i in range(len(cart)):
+            sub_total = prices[i] * amounts[i]
+            total += sub_total
+        print(f'The total price of the items in the shopping cart is ${total:.2f}')
+    elif choice == '5':
+        choice = '5'
+    else:
+        print('That is not a valid input. Please enter a number 1-5.')
 
 
 print('Thank you. Goodbye.')
